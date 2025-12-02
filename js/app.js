307c9ac84d5584093d0505ec05508d8a24db9494
tiffanyrodr <tiff18448@gmail.com>
2025-12-01 19:24:23 -0600

    patrones

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+const patron1 = ["red", "green", "red", "green", "red", "green", "red", "green", "red", "green"];
+const patron2 = ["blue", "yellow", "blue", "yellow", "blue", "yellow", "blue", "yellow", "blue", "yellow"];
+const patron3 = ["green", "green", "red", "red", "green", "green", "red", "red", "green", "green"];
+const patron4 = ["yellow", "blue", "red", "yellow", "blue", "red", "yellow", "blue", "red", "yellow"];
+const patron5 = ["red", "red", "red", "red", "red", "green", "green", "green", "green", "green"];
+
+const patrones = [patron1, patron2, patron3, patron4, patron5];
+
 const app = document.getElementById("app");
 function crearEstructura() {
     const container = document.createElement("div");
